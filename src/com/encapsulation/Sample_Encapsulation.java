c448c9522f39ffec4ea57cbef96d5acfe793feb1
rishiramanathan <rishiramanathan1998@gmail.com>
2021-12-08 13:31:23 +0530

    Updated

---
+++ src/com/encapsulation/Sample_Encapsulation.java
@@ -0,0 +1,42 @@
+package com.encapsulation;
+
+public class Sample_Encapsulation {
+	
+	private void name1() {
+		
+		System.out.println("My name is R.Rishi");
+		
+		System.out.println("Age is 23 ");
+		
+		System.out.println("16umec007");
+		
+	}
+	
+private void name2() {
+		
+		System.out.println("My name is R.hari");
+		
+		System.out.println("Age is 22 ");
+		
+		System.out.println("16ece017");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Sample_Encapsulation a = new Sample_Encapsulation();
+		
+		a.name1();
+		
+		a.name2();
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
